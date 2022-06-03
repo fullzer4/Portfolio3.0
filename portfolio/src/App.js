d5926c2e50ac3abe0c017d3a0ed2a80486b182b1
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-03 09:09:00 -0300

    att

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component} from 'react';
+import "./styles/reset.css";
+import "./styles/App.css";
+
+class App extends Component {
+    render() {
+        return(
+            <div id="pagina">
+            </div>
+        )
+    }
+}
+
+export default App;
