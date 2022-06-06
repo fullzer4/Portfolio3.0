01b8fa66e44e385efec37f81e8e399e1baf884a4
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-06 11:50:50 -0300

    att

--- portfolio/src/components/HomeHeader.js
+++ portfolio/src/components/HomeHeader.js
@@ -5,9 +5,6 @@ export class HomeHeader extends Component {
     render() {
         return(
             <div id="Home-Header">
-                <div className="site-nome">
-                    <h1>Fullzer4s Website</h1>
-                </div>
                 <div className="header-github">
                     <img src={github} alt="github"/>
                 </div>
