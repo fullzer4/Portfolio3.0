816b3ed6bd3cea0a642b797151721a37f931b57b
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-06 10:02:41 -0300

    att

--- portfolio/src/components/HomeHover.js
+++ portfolio/src/components/HomeHover.js
@@ -1,11 +1,30 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import Arrow from "../imgs/arrow.svg"
 
 export class HomeHover extends Component {
+    constructor(props) {
+        super(props);
+        this.hover = this.hover.bind(this);
+        
+    }
+
+    hover() {
+        alert("puta")
+        document.getElementsByClassName("hover")[0].classList.add("active")
+    }
+    fechar() {
+        alert("puta")
+        document.getElementsByClassName("hover")[0].classList.remove("active")
+    }
     render() {
         return(
-            <div id="botao-Hover">
-                <button><img src={Arrow} alt="botao"></img></button>
+            <div id="box-hover">
+                <div className="hover">
+                    <button id="botao-hover-close" onClick={this.fechar}></button>
+                </div>
+                <div id="box-botao-hover">
+                    <button id="botao-hover" onClick={this.hover}><img src={Arrow} alt="botao"></img></button>
+                </div>
             </div>
         )
     }
