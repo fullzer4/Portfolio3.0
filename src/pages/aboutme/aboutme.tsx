c5a33c9a55f8ab3feda8c3e3de691abc805833bd
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-09 22:13:34 -0300

    att

---
+++ src/pages/aboutme/aboutme.tsx
@@ -0,0 +1,7 @@
+export default function Aboutme() {
+    return(
+      <div>
+          <h1>about</h1>
+      </div>
+    )
+  }
--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route} from "react-router-dom"
 import Certificates from "../pages/certificates/certificates"
 import Home from "../pages/home/home"
 import Projects from "../pages/projects/projects"
+import Aboutme from "../pages/aboutme/aboutme"
 
 export default function Rota() {
   return(
@@ -10,6 +11,7 @@ export default function Rota() {
         <Route path="/" element={<Home/>}/>
         <Route path="/projects" element={<Projects/>}/>
         <Route path="/certificates" element={<Certificates/>}/>
+        <Route path="/aboutme" element={<Aboutme/>}/>
       </Routes>
     </BrowserRouter>
   )
