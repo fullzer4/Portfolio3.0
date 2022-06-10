cbc58267409577c62b1633b58e9c1af8e1e63571
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-09 21:43:10 -0300

    att

---
+++ src/components/bemvindo.tsx
@@ -0,0 +1,7 @@
+export default function Bemvindo() {
+    return(
+      <div>
+          <h2>cert</h2>
+      </div>
+    )
+  }
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,10 @@
+import Bemvindo from "../../components/bemvindo"
+
 export default function Home() {
     return(
       <div>
           <h1>oi</h1>
+          <Bemvindo/>
       </div>
     )
   }
