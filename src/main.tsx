cff0c72fa9a50712eb28d6d524c2aab3a68a624a
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-12 13:25:18 -0300

    att

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { MenuContextProvider } from './contexts/menu'
 import Rota from './routes/route'
 import "./index.css"
+import 'animate.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
