bdb484e9d42bd7d7431db671bb115c44ab613012
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-15 00:57:21 -0300

    att

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,6 @@
 import Bemvindo from "../../components/bemvindo/bemvindo"
 import ButtonMenu from "../../components/menu/buttonMenu"
+import RedeSocial from "../../components/redesocial/redesocial"
 import Slider from "../../components/slider/slider"
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
       <ButtonMenu>
         <Bemvindo/>
         <Slider/>
+        <RedeSocial/>
       </ButtonMenu>
     )
   }
