6957deff576cb68bf4a3079f25f68aca7c532766
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-16 19:47:16 -0300

    att

--- src/components/socialCards/socialCards.tsx
+++ src/components/socialCards/socialCards.tsx
@@ -13,13 +13,13 @@ export default function SocialCards(){
         <div className="cards">
             <div className="card-discord">
                 <h1 className="card-titulo">{servername}</h1>
-                <img src={imgdiscord} alt="imagem server discord"/>
+                <img src={imgdiscord} alt="imagem server discord" className="card-imagens"/>
                 <h2 className="card-descri">users online: {onlineusers}</h2>
                 <a href={invite} className="card-botao">Entrar</a>
             </div>
             <div className="card-youtube">
                 <h1 className="card-titulo">Canal do youtube</h1>
-                <img src={imgyoutube} alt="imagem de perfil youtube"/>
+                <img src={imgyoutube} alt="imagem de perfil youtube" className="card-imagens"/>
                 <h2 className="card-descri">yFullzer4</h2>
                 <a href="https://www.youtube.com/channel/UCKsvtOneTEJm9z80MbbfKfg" className="card-botao">Entrar</a>
             </div>
