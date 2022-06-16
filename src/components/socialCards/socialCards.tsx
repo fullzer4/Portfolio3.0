e4a27ff3de5ce9468518c0ce52dddc27f1349572
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-16 18:24:55 -0300

    att

--- src/components/socialCards/socialCards.tsx
+++ src/components/socialCards/socialCards.tsx
@@ -1,5 +1,8 @@
 import { useContext } from "react"
 import {ApiDiscordContext} from "../../contexts/apiDiscord"
+import "./socialcards.css"
+import imgdiscord from "../../imgs/serverdisc.svg"
+import imgyoutube from "../../imgs/youtubeicon.svg"
 
 export default function SocialCards(){
     const {fdiscord, servername, invite, onlineusers} = useContext(ApiDiscordContext)
@@ -9,12 +12,16 @@ export default function SocialCards(){
     return(
         <div className="cards">
             <div className="card-discord">
-                <h1>{servername}</h1>
-                <h2>usuarios online: {onlineusers}</h2>
-                <h3>entrar: <a href={invite}>link</a></h3>
+                <h1 className="card-titulo">{servername}</h1>
+                <img src={imgdiscord} alt="imagem server discord"/>
+                <h2 className="card-descri">users online: {onlineusers}</h2>
+                <a href={invite} className="card-botao">Entrar</a>
             </div>
             <div className="card-youtube">
-
+                <h1 className="card-titulo">Canal do youtube</h1>
+                <img src={imgyoutube} alt="imagem de perfil youtube"/>
+                <h2 className="card-descri">yFullzer4</h2>
+                <a href="https://www.youtube.com/channel/UCKsvtOneTEJm9z80MbbfKfg" className="card-botao">Entrar</a>
             </div>
         </div>
     )
