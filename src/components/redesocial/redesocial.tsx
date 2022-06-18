ecbca7600bf723c52ac5f3c031f5b4fe3b44fead
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-18 18:03:47 -0300

    att

--- src/components/redesocial/redesocial.tsx
+++ src/components/redesocial/redesocial.tsx
@@ -3,19 +3,14 @@ import instagram from "../../imgs/instagram.svg"
 import linkedin from "../../imgs/linkedin.svg"
 import twitch from "../../imgs/twitch.svg"
 import "./redesocial.css"
-import AOS from "aos"
 
 export default function RedeSocial() {
-  AOS.init({
-    delay: 100,
-    duration: 200
-  })
   return(
     <div className="box-icons">
-      <img src={github} alt="github" className="icons" data-aos="fade-up"/>
-      <img src={instagram} alt="instagram" className="icons" data-aos="fade-up"/>
-      <img src={linkedin} alt="linkedin" className="icons" data-aos="fade-up"/>
-      <img src={twitch} alt="twitch" className="icons" data-aos="fade-up"/>
+      <img src={github} alt="github" className="icons"/>
+      <img src={instagram} alt="instagram" className="icons"/>
+      <img src={linkedin} alt="linkedin" className="icons"/>
+      <img src={twitch} alt="twitch" className="icons"/>
     </div>
   )
 }
