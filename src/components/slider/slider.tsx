9e13370d77003917792ba2d4022b72575aa0016b
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-18 13:58:07 -0300

    att

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -19,9 +19,15 @@ export default function Slider() {
                         <button className="slide-Botao">Ver mais</button>
                     </div>
                 </div>
-            </div>
-            <div className="slide-change-button">
-
+                <div className="slide1">
+                    <img src={idcSlide} data-aos="fade-right" alt="imagem Bone para defecientes visuais feito na idocode" className="img"/>
+                    <div className="slide-content-box" data-aos="fade-up">
+                        <h2 className="slide-Titulo">Bone para deficientes visuais</h2>
+                        <div className="slide-Divisor"></div>
+                        <h3 className="slide-Descricao">Esse projeto foi feito na start up Idocode com o foco de ajudar deficientes visuais. Para realizar foi usado raspeberripy e sonar com a linguagem python.</h3>
+                        <button className="slide-Botao">Ver mais</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
