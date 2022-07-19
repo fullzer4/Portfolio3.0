d2654e28ec70717a3fe9f62319e07b93ed578c72
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-07-19 01:29:37 -0300

    att

--- src/components/Incial/Incial.tsx
+++ src/components/Incial/Incial.tsx
@@ -3,13 +3,10 @@ import "./Incial.css"
 export default function Inicial() {
     return(
       <div id="box-inicial">
-            <div className="box-fundo">
-                <div className="box-inicial-dark"/>
-                <div className="box-titulo">
-                  <h1>Portfolio</h1>
-                  <h2>Gabriel Pelizzaro PereiraFull-Stack Developer</h2>
-                </div>
-            </div>
+        <div className="box-fundo">
+          <h1>Portfolio</h1>
+          <h2>Gabriel Pelizzaro PereiraFull-Stack Developer</h2>
+        </div>
       </div>
     )
   }
