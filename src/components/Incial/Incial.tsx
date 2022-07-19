5a8617a05611540a893deae1cf5557ca4014c61d
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-07-19 02:40:13 -0300

    att

--- src/components/Incial/Incial.tsx
+++ src/components/Incial/Incial.tsx
@@ -6,6 +6,11 @@ export default function Inicial() {
         <div className="box-fundo">
           <h1>Portfolio</h1>
           <h2>Gabriel Pelizzaro PereiraFull-Stack Developer</h2>
+          <div className="box-icons">
+            <img className="box-icons-img"></img>
+            <img className="box-icons-img"></img>
+            <img className="box-icons-img"></img>
+          </div>
         </div>
       </div>
     )
