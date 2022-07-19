4944c21c03fb7ef6e88d4313fb54fad3973014e1
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-07-19 03:05:39 -0300

    att

--- src/components/Incial/Incial.tsx
+++ src/components/Incial/Incial.tsx
@@ -1,15 +1,20 @@
+import Github from "../../imgs/github.svg"
+import Linkedin from "../../imgs/linkedin.svg"
+import Instagram from "../../imgs/instagram.svg"
 import "./Incial.css"
+import { Link } from "react-router-dom"
 
 export default function Inicial() {
+
     return(
       <div id="box-inicial">
         <div className="box-fundo">
           <h1>Portfolio</h1>
           <h2>Gabriel Pelizzaro PereiraFull-Stack Developer</h2>
           <div className="box-icons">
-            <img className="box-icons-img"></img>
-            <img className="box-icons-img"></img>
-            <img className="box-icons-img"></img>
+            <img className="box-icons-img" src={Github}></img>
+            <img className="box-icons-img" src={Linkedin}></img>
+            <img className="box-icons-img" src={Instagram}></img>
           </div>
         </div>
       </div>
