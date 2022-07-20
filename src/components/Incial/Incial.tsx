4c631db853d7d94cbcf37fcf7e3e800f64c9ebe0
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-07-20 00:06:06 -0300

    att

--- src/components/Incial/Incial.tsx
+++ src/components/Incial/Incial.tsx
@@ -1,8 +1,8 @@
 import Github from "../../imgs/github.svg"
 import Linkedin from "../../imgs/linkedin.svg"
 import Instagram from "../../imgs/instagram.svg"
+import Seta from "../../imgs/seta.svg"
 import "./Incial.css"
-import { Link } from "react-router-dom"
 
 export default function Inicial() {
 
@@ -10,11 +10,15 @@ export default function Inicial() {
       <div id="box-inicial">
         <div className="box-fundo">
           <h1>Portfolio</h1>
+          <hr/>
           <h2>Gabriel Pelizzaro PereiraFull-Stack Developer</h2>
           <div className="box-icons">
-            <img className="box-icons-img" src={Github}></img>
-            <img className="box-icons-img" src={Linkedin}></img>
-            <img className="box-icons-img" src={Instagram}></img>
+            <a href="https://github.com/fullzer4"><img className="box-icons-img" src={Github}/></a>
+            <a href="https://www.linkedin.com/in/gabrielpereira-sc/"><img className="box-icons-img" src={Linkedin}/></a>
+            <a href="https://www.instagram.com/yfullzer4.exe/"><img className="box-icons-img" src={Instagram}/></a>
+          </div>
+          <div className="box-seta"> 
+            <img src={Seta}></img>
           </div>
         </div>
       </div>
