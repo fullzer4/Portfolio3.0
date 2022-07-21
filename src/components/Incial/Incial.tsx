ba9b86ec50dac7a0ecd53d810980d412b240e1b6
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-07-21 02:58:20 -0300

    att

--- src/components/Incial/Incial.tsx
+++ src/components/Incial/Incial.tsx
@@ -9,12 +9,12 @@ export default function Inicial() {
     return(
       <div id="box-inicial">
         <div className="box-fundo">
-          <h1 className="inicial-titulo">Portfolio</h1>
+          <h1 className="inicial-titulo">Portfólio</h1>
           <hr className="inicial-traco"/>
           <h2 className="inicial-descricao">Gabriel Pelizzaro Pereira Full-Stack Developer</h2>
           <div className="box-icons">
-            <a href="https://github.com/fullzer4"><img className="box-icons-img" src={Github}/></a>
             <a href="https://www.linkedin.com/in/gabrielpereira-sc/"><img className="box-icons-img" src={Linkedin}/></a>
+            <a href="https://github.com/fullzer4"><img className="box-icons-img" src={Github}/></a>
             <a href="https://www.instagram.com/yfullzer4.exe/"><img className="box-icons-img" src={Instagram}/></a>
           </div>
           <div className="box-seta"> 
