31cfa9ea907c880cc9d7c8e1d8133be544ec232d
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-07-29 02:28:55 -0300

    att

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,6 +1,6 @@
 export default function App() {
   return(
-    <div>
+    <div id="conteudo">
       
     </div>
   )
---
+++ site/src/components/entrada.tsx
@@ -0,0 +1,7 @@
+export default function Entrada() {
+    return(
+      <div id="entrada">
+        
+      </div>
+    )
+  }
