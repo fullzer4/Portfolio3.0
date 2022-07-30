9d0d0d023341599feef39a96d8346ac220b265ae
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-07-30 03:38:14 -0300

    att

--- site/src/components/entrada/entrada.tsx
+++ site/src/components/entrada/entrada.tsx
@@ -1,5 +1,7 @@
 import './entrada.scss'
-
+import github from "../../assets/github.svg"
+import linkedin from "../../assets/github.svg"
+import instagram from "../../assets/github.svg"
 export default function Entrada() {
     return(
       <div id="entrada">
@@ -11,9 +13,9 @@ export default function Entrada() {
           <p>Gabriel Pelizzaro Pereira<br/>Full-Stack Developer</p>
         </div>
         <div className="icons-entrada">
-          <div className="icons-github"></div>
-          <div className="icons-linkedin"></div>
-          <div className="icons-instagram"></div>
+          <img className="icons-github" src={github}></img>
+          <img className="icons-linkedin" src={linkedin}></img>
+          <img className="icons-instagram" src={instagram}></img>
         </div>
         <div className="seta-entrada"></div>
       </div>
