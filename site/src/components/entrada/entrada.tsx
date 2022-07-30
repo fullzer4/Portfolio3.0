082a4825d09fbe80a75e54c171df746fa3beb78d
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-07-30 00:44:28 -0300

    att

--- site/src/components/entrada/entrada.tsx
+++ site/src/components/entrada/entrada.tsx
@@ -3,7 +3,17 @@ import './entrada.scss'
 export default function Entrada() {
     return(
       <div id="entrada">
-        
+        <div className="texto-entrada">
+          <h1>Portfolio</h1>
+          <hr/>
+          <h2>Gabriel Pelizzaro Pereira <p/> Full-Stack Developer</h2>
+        </div>
+        <div className="icons-entrada">
+          <div className="icons-github"></div>
+          <div className="icons-linkedin"></div>
+          <div className="icons-instagram"></div>
+        </div>
+        <div className="seta-entrada"></div>
       </div>
     )
   }
