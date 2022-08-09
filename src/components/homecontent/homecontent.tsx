5c09d473d357141a5b59c346c0526428e170d17f
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-08-08 23:53:28 -0300

    att

--- src/components/homecontent/homecontent.tsx
+++ src/components/homecontent/homecontent.tsx
@@ -3,7 +3,11 @@ import "./homecontent.scss"
 export default function Homecontent(){
     return(
         <div className="homecontent">
-            
+            <div className="homecontent-text">
+                <h1>Bem vindo ao meu portfolio</h1>
+                <p>Gabriel Pelizzaro Pereira, FullStack Developer and IOT</p>
+            </div>
+            <div className="homecontent-image"/>
         </div>
     )
 }
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,9 +1,11 @@
 import Animationhome from "../../components/animationhome/animationhome";
+import Homecontent from "../../components/homecontent/homecontent";
 
 export default function home(){
     return(
         <div>
             <Animationhome/>
+            <Homecontent/>
         </div>
     )
 }
