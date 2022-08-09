362d2c50063febfc65d8fd98be3f4ef03dff1942
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-08-09 00:01:13 -0300

    att

--- src/components/homecontent/homecontent.tsx
+++ src/components/homecontent/homecontent.tsx
@@ -4,8 +4,8 @@ export default function Homecontent(){
     return(
         <div className="homecontent">
             <div className="homecontent-text">
-                <h1>Bem vindo ao meu portfolio</h1>
-                <p>Gabriel Pelizzaro Pereira, FullStack Developer and IOT</p>
+                <h1>Bem vindo ao meu portfólio</h1>
+                <p>Sou o Gabriel Pelizzaro Pereira, tenho 16 anos e moro em Florianópolis/SC, sou um FullStack Developer and IOT developer.</p>
             </div>
             <div className="homecontent-image"/>
         </div>
