f7d1812cfc46416dc17f3d6e9a855347cd39830d
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-08-18 15:36:42 -0300

    att

--- src/services/apigithub.tsx
+++ src/services/apigithub.tsx
@@ -7,12 +7,23 @@ export const GithubContext = createContext<any>(()=>{})
 export const GithubProvider = ({ children }: any) => {
 
     const [user, setUser] = useState("")
-    
+    const [followers, setFollowers] = useState("")
+    const [url, setUrl] = useState("")
+    const [repos, setRepos] = useState("")
+    const [avatar, setAvatar] = useState("")
+
+
+
+
     function pegardados(){
         axios.get('https://api.github.com/users/fullzer4').then(resp => {
     
         console.log(resp.data);
         setUser(resp.data.login)
+        setFollowers(resp.data.followers)
+        setUrl(resp.data.html_url)
+        setRepos(resp.data.public_repos)
+        setAvatar(resp.data.avatar_url)
         console.log(user)
         });
     }
