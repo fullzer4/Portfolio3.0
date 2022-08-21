ad08ed12535b086c5115c736ee603605694cae15
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-08-21 18:38:39 -0300

    att

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,15 @@
+import "../scss/navbar.scss"
+
 export default function Navbar() {
 
     return (
         <div className="Navbar">
-            <div className="Navbar-nav"></div>
+            <div className="Navbar-nav">
+                <h1>foi</h1>
+            </div>
+            <div className="Theme-button">
+                <h1>foi</h1>
+            </div>
         </div>
     )
   }
