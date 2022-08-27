a46a203f1ef9cb65af2b8cacc399d2819dcd4a70
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-08-27 19:36:00 -0300

    att

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,18 @@
 import { useContext } from "react"
+import { SwitchLanguageContext } from "../contexts/switchlanguage"
 import { SwitchThemeContext } from "../contexts/switchtheme"
 import "../scss/navbar.scss"
 
 export default function Navbar() {
     const {mudarcor} = useContext(SwitchThemeContext)
+    const {langswitcher, lang, imglang} = useContext(SwitchLanguageContext)
+
     return (
         <div className="Navbar">
+            <div onClick={langswitcher}>
+                    <div className={imglang}/>
+                    <p>{lang}</p>
+                </div>
             <div className="Navbar-nav">
                 <h1>foi</h1>
             </div>
