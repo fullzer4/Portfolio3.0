eeaa306ac358922e46ac91ced690f6c42b062d8e
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-08-31 08:47:58 -0300

    att

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react"
 import { NavbarSelectContext } from "../contexts/navbarpageselect"
+import "../scss/home.scss"
 
 export default function Home() {
     
@@ -9,11 +10,14 @@ export default function Home() {
         <div className="Home">
             <div className="Resumo-Home">
                 <div className="Resumo-texto">
-                    <h2>Bem vindo</h2>
+                    <h2 className="Resumo-texto-titulo">Bem vindo</h2>
                     <p>Sou o Gabriel tenho 16 anos e este é o meu portfólio espero que goste, qualquer coisa só entrar em contato comigo.</p>
                     <button onClick={() => pageselector(2)}> Mais sobre mim </button>
                 </div>
                 <div className="Resumo-imagem"/>
+            </div>
+            <div className="Resumo-Redes">
+                
             </div>
         </div>
     )
