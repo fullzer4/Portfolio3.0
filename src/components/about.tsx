914deac1ce891f421fe57ba2e8feda192a92c2c3
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-09-02 09:53:08 -0300

    att

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,13 +1,19 @@
+import { useContext } from "react"
+import { SwitchLanguageContext } from "../contexts/switchlanguage"
+import "../scss/about.scss"
+
 export default function About() {
+
+    const {tituloaboutme, descricaoaboutme, buttonaboutme} = useContext(SwitchLanguageContext)
 
     return (
         <div className="About">
-            <div>
-                <div/>
-                <div>
-                    <h2>Mais sobre mim</h2>
-                    <p>Esse de camiseta branca sou eu em 2020 e foi ai quando eu começei a me apaixonar por programação, começei a estudar programação em 2018 numa start-up chamada IDoCode e sinceramente no inicio eu não gostava de programação mas em 2019 quando eu começei a fazer projetos junto com meus amigos acabei decidindo que esse ia ser o foco para a minha vida.</p>
-                    <button>Ver projetos</button>
+            <div className="About-me">
+                <div className="About-me-image"/>
+                <div className="About-me-texto">
+                    <h2 className="About-me-texto-titulo">{tituloaboutme}</h2>
+                    <p>{descricaoaboutme}</p>
+                    <button>{buttonaboutme}</button>
                 </div>
             </div>
         </div>
--- src/contexts/switchlanguage.tsx
+++ src/contexts/switchlanguage.tsx
@@ -12,8 +12,9 @@ export const SwitchLanguageProvider = ({ children }: any) => {
     const [cardtexthomegithub, setCardtexthomegithub] = useState("Onde eu posto meus codigos e dou um resumo dos meu projetos, atualmente tenho")
     const [cardtexthomeyoutube, setCardtexthomeyoutube] = useState("Criei esse canal para ajudar pessoas em programação, hoje tenho postado mais de")
     const [cardbutton, setCardbutton] = useState("Entrar")
-
-
+    const [tituloaboutme, setTituloaboutme] = useState("Mais sobre mim")
+    const [descricaoaboutme, setDescricaoaboutme] = useState("Esse de camiseta branca sou eu em 2020 e foi ai quando eu comecei a me apaixonar por programação, comecei a estudar programação em 2018 numa start-up chamada IDoCode e sinceramente no início eu não gostava de programação mas em 2019 quando eu comecei a fazer projetos junto com meus amigos acabei decidindo que esse ia ser o foco para a minha vida.")
+    const [buttonaboutme, setButtonaboutme] = useState("Ver projetos")
 
     function langswitcher(){
         if(lang === "PT-BR"){
@@ -26,6 +27,9 @@ export const SwitchLanguageProvider = ({ children }: any) => {
             setCardtexthomegithub("Where I post my codes and give a summary of my projects, I currently have")
             setCardtexthomeyoutube("I created this channel to help people with coding, today I have posted more than")
             setCardbutton("Join")
+            setTituloaboutme("More about me")
+            setDescricaoaboutme("The one in the white t-shirt is me in 2020 and that's when I started to fall in love with programming, I started studying programming in 2018 at a start-up called IDoCode and honestly at first I didn't like programming but in 2019 when I started doing projects with my friends I ended up deciding that this was going to be the focus for my life.")
+            setButtonaboutme("View projects")
         }else if(lang === "ENG"){
             setLang("PT-BR")
             setImglang("brazil-icon")
@@ -36,6 +40,9 @@ export const SwitchLanguageProvider = ({ children }: any) => {
             setCardtexthomegithub("Onde eu posto meus codigos e dou um resumo dos meu projetos, atualmente tenho")
             setCardtexthomeyoutube("Criei esse canal para ajudar pessoas em programação, hoje tenho postado mais de")
             setCardbutton("Entrar")
+            setTituloaboutme("Mais sobre mim")
+            setDescricaoaboutme("Esse de camiseta branca sou eu em 2020 e foi ai quando eu comecei a me apaixonar por programação, comecei a estudar programação em 2018 numa start-up chamada IDoCode e sinceramente no início eu não gostava de programação mas em 2019 quando eu comecei a fazer projetos junto com meus amigos acabei decidindo que esse ia ser o foco para a minha vida.")
+            setButtonaboutme("Ver projetos")
         }else{
             setLang("PT-BR")
             setImglang("brazil-icon")
@@ -46,6 +53,9 @@ export const SwitchLanguageProvider = ({ children }: any) => {
             setCardtexthomegithub("Onde eu posto meus codigos e dou um resumo dos meu projetos, atualmente tenho")
             setCardtexthomeyoutube("Criei esse canal para ajudar pessoas em programação, hoje tenho postado mais de")
             setCardbutton("Entrar")
+            setTituloaboutme("Mais sobre mim")
+            setDescricaoaboutme("Esse de camiseta branca sou eu em 2020 e foi ai quando eu comecei a me apaixonar por programação, comecei a estudar programação em 2018 numa start-up chamada IDoCode e sinceramente no início eu não gostava de programação mas em 2019 quando eu comecei a fazer projetos junto com meus amigos acabei decidindo que esse ia ser o foco para a minha vida.")
+            setButtonaboutme("Ver projetos")
         }
     }
 
@@ -61,6 +71,9 @@ export const SwitchLanguageProvider = ({ children }: any) => {
             cardtexthomediscord,
             cardtexthomegithub,
             cardtexthomeyoutube,
+            tituloaboutme,
+            descricaoaboutme,
+            buttonaboutme
             }}> 
             {children}
         </SwitchLanguageContext.Provider>
