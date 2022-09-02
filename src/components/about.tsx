fade385e0b7e5d5a4243a16be1081ae472e22356
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-09-02 09:56:37 -0300

    att

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,9 +1,11 @@
 import { useContext } from "react"
+import { NavbarSelectContext } from "../contexts/navbarpageselect"
 import { SwitchLanguageContext } from "../contexts/switchlanguage"
 import "../scss/about.scss"
 
 export default function About() {
 
+    const {pageselector} = useContext(NavbarSelectContext)
     const {tituloaboutme, descricaoaboutme, buttonaboutme} = useContext(SwitchLanguageContext)
 
     return (
@@ -13,7 +15,7 @@ export default function About() {
                 <div className="About-me-texto">
                     <h2 className="About-me-texto-titulo">{tituloaboutme}</h2>
                     <p>{descricaoaboutme}</p>
-                    <button>{buttonaboutme}</button>
+                    <button onClick={() => pageselector(3)}>{buttonaboutme}</button>
                 </div>
             </div>
         </div>
