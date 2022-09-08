1636cc83278b57e31ba515f6aca70dee3c5fb610
Gabriel Pelizzaro Pereira <53914594+fullzer4@users.noreply.github.com>
2022-09-07 23:31:41 -0300

    att

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -20,6 +20,49 @@ export default function About() {
                     <button className={buttonprojetosobre} onClick={() => pageselector(3)}>{buttonaboutme}</button>
                 </div>
             </div>
+            <div  className="Tencinal-Habilities">
+                <div  className="Tencinal-Habilities-title">
+                    <p></p>
+                </div>
+                <div  className="Tencinal-Habilities-itens">
+                    <div className="coluna-esq">
+                        <div className="iten">
+                            <p>HTML</p>
+                            <div><div></div></div>
+                        </div>
+                        <div className="iten">
+                            <p>CSS</p>
+                            <div><div></div></div>
+                        </div>
+                        <div className="iten">
+                            <p>SCSS</p>
+                            <div><div></div></div>
+                        </div>
+                        <div className="iten">
+                            <p>Javascript</p>
+                            <div><div></div></div>
+                        </div>
+                    </div>
+                    <div className="coluna-dir">
+                        <div className="iten">
+                            <p>Typescript</p>
+                            <div><div></div></div>
+                        </div>
+                        <div className="iten">
+                            <p>React</p>
+                            <div><div></div></div>
+                        </div>
+                        <div className="iten">
+                            <p>Python</p>
+                            <div><div></div></div>
+                        </div>
+                        <div className="iten">
+                            <p>Nodejs</p>
+                            <div><div></div></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
   }
