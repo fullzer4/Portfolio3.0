c97f6a55eec0b90ce9382db5fdb11b0634071177
Gabriel Pelizzaro Pereira <53914594+fullzer4@users.noreply.github.com>
2022-09-10 20:43:06 -0300

    att

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -2,7 +2,9 @@ export default function Projects() {
 
     return (
         <div className="Projects">
-
+            <div>
+                <iframe src="https://tecsasenaictai.netlify.app/" scrolling="no"></iframe>
+            </div>
         </div>
     )
   }
