52f075711f9d1fbfce58a9086ca56563271c182a
Gabriel Pelizzaro Pereira <53914594+fullzer4@users.noreply.github.com>
2022-09-17 20:56:28 -0300

    att

--- src/components/certificados.tsx
+++ src/components/certificados.tsx
@@ -1,8 +1,12 @@
+import "../scss/certificados.scss"
+
 export default function Certificados() {
 
     return (
         <div className="Certificados">
- 
+            <div className="Cert-titulo">
+                <h2>Certificados</h2>
+            </div>
         </div>
     )
   }
